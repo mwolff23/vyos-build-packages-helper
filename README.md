3ec3c194b5af219249c2f1ccdafca495829bae46
mwolff <mwolff@build.wlg.afx.ovh>
2024-09-01 23:06:15 +0200

    Add equuleus

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ You will be dropped in a vyos-build shell with a webserver at localhost serving 
 Example usage:
 
 ```
-sudo ./build-vyos-image iso \
+./configure \
   --architecture amd64 \
   --build-by "root@localhost" \
   --build-type release \
-  --version "1.4.x-$(date +%s)" \
+  --version "1.3.x-$(date +%s)" \
   --vyos-mirror "http://localhost/dev.packages.vyos.net" \
   --custom-apt-key /vyos/apt/pubkey.asc
+
+sudo make iso
 ```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,11 @@ sudo rm -rf /var/www/html
 sudo ln -s /vyos/apt/pub /var/www/html
 sudo lighttpd -f /etc/lighttpd/lighttpd.conf
 
-[ -e /vyos/vyos-build ] || git clone -b sagitta /vyos/pkg_build/vyos-build /vyos/vyos-build
+[ -e /vyos/vyos-build ] || git clone -b equuleus /vyos/pkg_build/vyos-build /vyos/vyos-build
 cd /vyos/vyos-build
 
 echo ''
-echo 'vyos/vyos-build:sagitta'
+echo 'vyos/vyos-build:equuleus'
 echo 'Local apt repo reachable at:'
 echo 'http://localhost/dev.packages.vyos.net'
 echo ''
